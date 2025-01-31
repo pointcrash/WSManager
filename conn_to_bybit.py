0d54451c2ad9fe24cdd099830c380ee8d587d172
pointcrash <r.s.ricce@gmail.com>
2025-01-31 14:06:56 +0100

    \

--- conn_to_bybit.py
+++ conn_to_bybit.py
@@ -16,6 +16,8 @@ from global_variables import bybit_ws_private, bybit_ws_public, queue_dict
 
 
 def get_session(account):
+    print(f"From get_session account data: {account} ")
+
     session = HTTP(
         testnet=False,
         demo=account.testnet,
